88d9c10707b3fe0b575683620a865d059f82751e
webwithyaroo <olawaleridollahi@gmail.com>
2025-08-05 01:01:12 -0700

    chore: initial project setup with essential dependencies and configurations

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">this is from the web app</h1>
+        <h1 className="text-2xl font-bold">this is from the widget app </h1>
         <Button size="sm">Button</Button>
-        <div>{add(1, 2)}</div>
+        <Input placeholder="Input" />
       </div>
     </div>
   );
